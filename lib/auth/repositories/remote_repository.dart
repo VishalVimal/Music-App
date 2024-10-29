f909d71d55acb82cb60e31e5fad325cbac61e913
Vishal <www.vishalvimal@gmail.com>
2024-10-29 08:31:53 +0530

    error handling using try and catch

--- lib/auth/repositories/remote_repository.dart
+++ lib/auth/repositories/remote_repository.dart
@@ -14,56 +14,66 @@ class AuthRemoteRepository {
       throw ('Name, email, and password cannot be empty or contain only spaces.');
     }
 
-    final response = await http.post(
-      Uri.parse(
-        'http://10.0.2.2:8000/auth/signup', // Update this line
-      ),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode(
-        {
-          'name': name, //simple map
-          'email': email,
-          'password': password
+    try {
+      final response = await http.post(
+        Uri.parse(
+          'http://10.0.2.2:8000/auth/signup', // Update this line
+        ),
+        headers: {
+          'Content-Type': 'application/json',
         },
-      ),
-    );
+        body: jsonEncode(
+          {
+            'name': name, //simple map
+            'email': email,
+            'password': password
+          },
+        ),
+      );
 
-    // Error handling
-    if (response.statusCode != 201) {
-      throw Exception('Failed to sign up: ${response.body}');
+      // Error handling
+      if (response.statusCode != 201) {
+        throw Exception('Failed to sign up: ${response.body}');
+      }
+
+      print(response.body);
+      print(response.statusCode);
+    } //async - asynchronous is used bcz of http plugin here
+    catch (e) {
+      print(e);
     }
-
-    print(response.body);
-    print(response.statusCode);
-  } //async - asynchronous is used bcz of http plugin here
-
-  Future<void> login({required String email, required String password}) async {
-    // Validation to check for blank spaces
-    if (email.trim().isEmpty || password.trim().isEmpty) {
-      throw ('Email and password cannot be empty or contain only spaces.');
     }
+    
 
-    final response = await http.post(
-      Uri.parse(
-        'http://10.0.2.2:8000/auth/login',
-      ),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode(
-        {
-          'email': email,
-          'password': password,
-        },
-      ),
-    );
-    print(response.body);
-    print(response.statusCode);
+    Future<void> login(
+        {required String email, required String password}) async {
+      // Validation to check for blank spaces
+      if (email.trim().isEmpty || password.trim().isEmpty) {
+        throw ('Email and password cannot be empty or contain only spaces.');
+      }
+      try {
+        final response = await http.post(
+          Uri.parse(
+            'http://10.0.2.2:8000/auth/login',
+          ),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: jsonEncode(
+            {
+              'email': email,
+              'password': password,
+            },
+          ),
+        );
+        print(response.body);
+        print(response.statusCode);
 
-    if (response.statusCode != 200) {
-      throw Exception('Failed to sign in: ${response.body}');
+        if (response.statusCode != 200) {
+          throw Exception('Failed to sign in: ${response.body}');
+        }
+      } catch (e) {
+        print(e);
+      }
     }
   }
-}
